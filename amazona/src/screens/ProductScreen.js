567ea3075e18699259713d060c5ec6d27f1d2e36
Greg <litm47@gmail.com>
2023-04-04 13:51:31 -0400

    added loading spinner and error message

--- amazona/src/screens/ProductScreen.js
+++ amazona/src/screens/ProductScreen.js
@@ -9,6 +9,10 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from "react-bootstrap/Button";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import  { getError }  from "../utils";
+
 
 
 const reducer = (state, action) => {
@@ -42,7 +46,7 @@ const ProductScreen = () => {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (error) {
-        dispatch({ type: "FETCH_FAIL", payload: error.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(error) });
       }
     };
     fetchData();
@@ -50,10 +54,10 @@ const ProductScreen = () => {
 
   console.log(product)
   return loading ? (
-    <div>loading...</div>
-  ) : error ? (
-    <div>{error}</div>
-  ) : (
+    <LoadingBox/>
+   ) : error ? (
+     <MessageBox variant="danger">{error}</MessageBox>  
+   ) : (
     <div>
       <Row>
         <Col md={6}>
