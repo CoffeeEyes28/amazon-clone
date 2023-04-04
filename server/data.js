bb33c58069713309d024e24f8e656552897de74a
Greg <litm47@gmail.com>
2023-04-04 14:46:44 -0400

    cart now checks for item quantity when adding and alerts when stock limit is reached

--- server/data.js
+++ server/data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
         {
+            _id: '1',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
             description: 'high quality shirt'
         },
         {
+            _id: '2',
             name: 'Adidas Fit Shirt',
             slug: 'adidas-fit-shirt',
             category: 'Shirts',
@@ -24,6 +26,7 @@ const data = {
             numReviews: 10,
             description: 'high quality product'
         }, {
+            _id: '3',
             name: 'Nike Slim Pant',
             slug: 'nike-slim-pant',
             category: 'Pants',
@@ -35,6 +38,7 @@ const data = {
             numReviews: 14,
             description: 'high quality pants'
         }, {
+            _id: '4',
             name: 'Puma Fit Pants',
             slug: 'puma-fit-pants',
             category: 'Pants',
